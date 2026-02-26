63d6dd8041de00b5f67cb2b61c2a994d4cb23458
Mustafa Batuhan Büber <mustafa.buber@std.bogazici.edu.tr>
2026-02-26 14:21:20 +0300

    Enhance it

--- batu-study/starter/index.js
+++ batu-study/starter/index.js
@@ -1,6 +1,7 @@
 console.log("Batuhan was here.");
 
 import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
 // We need 3 objects generally:
 // 1. Renderer, 2. Camera, 3. Scene
@@ -31,20 +32,41 @@ camera.position.z = 2;
 // Create scene
 const scene = new THREE.Scene();
 
+// Create orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
 // Add a simple icosahedron to the scene
 const geo = new THREE.IcosahedronGeometry(1.0, 2);
-const mat = new THREE.MeshBasicMaterial({
-    color: 0xccff, // light blue
+const mat = new THREE.MeshStandardMaterial({
+    color: 0xffffff, // white
+    flatShading: true,
 });
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
+
+// Add the same geometry with a different material
+const wireMat = new THREE.MeshBasicMaterial({
+    color: 0xffffff, // white
+    wireframe: true, 
+});
+const wireMesh = new THREE.Mesh(geo, wireMat);
+wireMesh.scale.setScalar(1.001);
+mesh.add(wireMesh);
+
+// Light
+const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+scene.add(hemiLight);
 
 function animate(t = 0) {
     // This request provide us a time difference between each frame,
     // so we can use it for animations
     requestAnimationFrame(animate);
 
-    mesh.scale.setScalar(Math.cos(t * 0.001 + 1.0));
+    //mesh.scale.setScalar(Math.cos(t * 0.001 + 1.0));
+    mesh.rotation.y = t * 0.0001;
+    controls.update();
 
     // RENDER
     renderer.render(scene, camera);
